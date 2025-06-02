422644c3306e94458bb2bdea0e4f63c4f5ca6cb0
Mahid Ahmad <mahid797@hotmail.com>
2025-06-02 06:06:38 -0400

    fix merge conflicts

--- src/app/documents/components/ShareLinkDialog.tsx
+++ src/app/documents/components/ShareLinkDialog.tsx
@@ -4,22 +4,13 @@ import { Box, Chip, Dialog, DialogContent, DialogTitle, IconButton } from '@mui/
 
 import { CheckIcon, CopyIcon, LinkIcon } from '@/icons';
 
-<<<<<<<< HEAD:src/app/documents/components/NewLinkDialog.tsx
-interface NewLinkDialogProps {
-========
 interface ShareLinkDialogProps {
->>>>>>>> dev:src/app/documents/components/ShareLinkDialog.tsx
 	linkUrl: string;
 	onClose: () => void;
 }
 
-<<<<<<<< HEAD:src/app/documents/components/NewLinkDialog.tsx
-export default function NewLinkDialog({ linkUrl, onClose }: NewLinkDialogProps) {
-	const [isLinkCopied, setIsLinkCopied] = React.useState(false);
-========
 export default function ShareLinkDialog({ linkUrl, onClose }: ShareLinkDialogProps) {
 	const [copied, setCopied] = useState(false);
->>>>>>>> dev:src/app/documents/components/ShareLinkDialog.tsx
 
 	function handleCopy() {
 		navigator.clipboard.writeText(linkUrl);
